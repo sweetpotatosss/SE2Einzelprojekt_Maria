31e92b6e2d3f372054104b9c5e30e15d30ee43ae
Мария <maria@MacBook-Air-Maria.local>
2026-03-14 18:53:52 +0100

    Tests update for new leaderboard sorting

--- src/test/kotlin/at/aau/serg/controllers/LeaderboardControllerTests.kt
+++ src/test/kotlin/at/aau/serg/controllers/LeaderboardControllerTests.kt
@@ -38,10 +38,10 @@ class LeaderboardControllerTests {
     }
 
     @Test
-    fun test_getLeaderboard_sameScore_CorrectIdSorting() {
-        val first = GameResult(1, "first", 20, 20.0)
-        val second = GameResult(2, "second", 20, 10.0)
-        val third = GameResult(3, "third", 20, 15.0)
+    fun test_getLeaderboard_sameScore_CorrectTimeSorting() {
+        val first = GameResult(1, "first", 20, 10.0)
+        val second = GameResult(2, "second", 20, 15.0)
+        val third = GameResult(3, "third", 20, 20.0)
 
         whenever(mockedService.getGameResults()).thenReturn(listOf(second, first, third))
 
